dd87b81b1c46c5d2133ec7fc27365ba3beca5e7e
Aegir Aideron <jhdcruz00@gmail.com>
2019-08-06 21:28:05 +0800

    Fixed Single Instance

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, Tray, Menu } = require('electron')
+const gotTheLock = app.requestSingleInstanceLock()
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -33,106 +34,105 @@ function createWindow() {
   })
 }
 
-const singleInstance = app.requestSingleInstanceLock()
-
-if (!singleInstance) {
+if (!gotTheLock) {
   app.quit()
 } else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
+  app.on('second-instance', () => {
     // Someone tried to run a second instance, we should focus our window.
-    if (BrowserWindow) {
-      if (BrowserWindow.isMinimized()) BrowserWindow.restore()
-      BrowserWindow.focus()
+    if (win) {
+      if (!win.isVisible()) win.show()
+      win.focus()
     }
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Wait until the app is ready
-app.once('ready', () => {
 
-  // Starts Browser
-  createWindow()
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Wait until the app is ready
+  app.once('ready', () => {
 
-  let tray = null
-
-  // Creates a tray
-  tray = new Tray('icon.ico')
-
-  // Context Menu
-  const contextMenu = Menu.buildFromTemplate([{
-      label: 'Show/Hide Desktop Messenger',
-      accelerator: 'CmdOrCtrl+Q',
-      click: function () {
-        win.isVisible() ? win.hide() : win.show()
-      }
-    },
-    {
-      label: 'Reload',
-      accelerator: 'CmdOrCtrl+R',
-      click: function () {
-        win.reload()
-      }
-    },
-    {
-      label: 'Clear Data/Logout',
-      accelerator: 'CmdOrCtrl+D',
-      click: function () {
-        win.webContents.session.clearStorageData(function () {
-          console.log('Data Cleaned');
-        })
-
-        app.relaunch() && app.quit()
-      }
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Quit',
-      accelerator: 'CmdOrCtrl+X',
-      click: function () {
-        app.quit()
-      }
-    }
-  ])
-
-  tray.on('double-click', () => {
-    win.isVisible() ? win.hide() : win.show()
-  })
-
-  tray.on('right-click', function () {
-    contextMenu()
-  })
-
-  win.on('show', () => {
-    tray.setHighlightMode('always')
-  })
-
-  win.on('hide', () => {
-    tray.setHighlightMode('never')
-  })
-
-  tray.setToolTip('Desktop Messenger')
-  tray.setContextMenu(contextMenu)
-
-})
-
-// Quit when all windows are closed.
-// app.on('window-all-closed', () => {
-     // On macOS it is common for applications and their menu bar
-     // to stay active until the user quits explicitly with Cmd + Q
-//   if (process.platform !== 'darwin') {
-//     app.quit()
-//   }
-// })
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+    // Starts Browser
     createWindow()
-    win.focus()
-  }
-})
+
+    let tray = null
+
+    // Creates a tray
+    tray = new Tray('icon.ico')
+
+    // Context Menu
+    const contextMenu = Menu.buildFromTemplate([{
+        label: 'Show/Hide Desktop Messenger',
+        accelerator: 'CmdOrCtrl+Q',
+        click: function () {
+          win.isVisible() ? win.hide() : win.show()
+        }
+      },
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function () {
+          win.reload()
+        }
+      },
+      {
+        label: 'Clear Data/Logout',
+        accelerator: 'CmdOrCtrl+D',
+        click: function () {
+          win.webContents.session.clearStorageData(function () {
+            console.log('Data Cleaned');
+          })
+
+          app.relaunch() && app.quit()
+        }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+X',
+        click: function () {
+          app.quit()
+        }
+      }
+    ])
+
+    tray.on('double-click', () => {
+      win.isVisible() ? win.hide() : win.show()
+    })
+
+    tray.on('right-click', function () {
+      contextMenu()
+    })
+
+    win.on('show', () => {
+      tray.setHighlightMode('always')
+    })
+
+    win.on('hide', () => {
+      tray.setHighlightMode('never')
+    })
+
+    tray.setToolTip('Desktop Messenger')
+    tray.setContextMenu(contextMenu)
+
+  })
+
+  // Quit when all windows are closed.
+  // app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  //   if (process.platform !== 'darwin') {
+  //     app.quit()
+  //   }
+  // })
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow()
+      win.focus()
+    }
+  })
